4a5372106853bd30f9515e18da77ac7158a4577f
Lee Marvin <ryenem8@gmail.com>
2020-10-14 20:16:25 -0700

    succesfully recieved information from push

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
+var database = require("./db/db.json")
+var notes = []
 
 // Sets up the Express App
 // =============================================================
@@ -14,9 +17,24 @@ app.use(express.json());
 
 // Routes
 // =============================================================
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
 
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+  });
 
+app.get("/api/notes", function(req, res) {
+  return res.json(database);
+  });
 
+app.post("/api/notes", function(req, res) {
+  var newNote = req.body;
+  database.push(newNote);
+  console.log(newNote);
+
+});
 
 
 
