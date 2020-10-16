7d9aa1e15727609b6fb91c17d55c5156aa917f8b
Lee Marvin <ryenem8@gmail.com>
2020-10-16 11:26:56 -0700

    simplified api get route

--- server.js
+++ server.js
@@ -4,38 +4,55 @@ var express = require("express");
 var path = require("path");
 var fs = require("fs");
 var database = require("./db/db.json")
-var notes = []
+const util = require("util");
 
-// Sets up the Express App
+
+// Set up the Express App
 // =============================================================
 var app = express();
 var PORT =process.env.PORT ||  3000;
 
-// Sets up the Express app to handle data parsing
+// Set up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 // =============================================================
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+//Reading the files in db.json
+app.get("/api/notes", function(req, res) {
+  res.json(database);
+  console.log(database);
   });
+  
+//Writing the files into db.json
+app.post("/api/notes", function(req, res) {})
+  var note = req.body;
+//   fs.readFile(path.join(__dirname,"./db/db.json"), "utf8", function(err, data) {
+//   var parsedData = JSON.parsserve(data);
+//   var combinedData = [newNote, ...parsedData];
+  
+  // });
+  // // writeFileAsync("./db.db.json", combinedData, "utf8")
+
+
+//  readFileAsync("./db/db.json", "utf8")
+//  .then((notes) => 
+//  [newNote, ...notes])
+//  .then(updatedNotes => console.log(updatedNotes))
+
+  
+//   res.json("Successfully added note")
+// });
 
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
   });
 
-app.get("/api/notes", function(req, res) {
-  return res.json(database);
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
   });
-
-app.post("/api/notes", function(req, res) {
-  var newNote = req.body;
-  database.push(newNote);
-  console.log(newNote);
-
-});
-
 
 
 app.listen(PORT, function() {
